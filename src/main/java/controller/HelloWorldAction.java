c9bd53f2970a5685b99137b2fcbe52610c2d1072
SoldierJVX <jv_rss@hotmail.com>
2019-11-15 21:35:15 -0300

    Implementação hello world

---
+++ src/main/java/controller/HelloWorldAction.java
@@ -0,0 +1,19 @@
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import model.MessageStore;
+
+public class HelloWorldAction extends ActionSupport {
+    private MessageStore messageStore;
+
+    public String execute() {
+        messageStore = new MessageStore() ;
+        
+        return SUCCESS;
+    }
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+}
