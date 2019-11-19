3177f709c1424db415b08576b397b1982e9728b9
joaosilva <jv_rss@hotmail.com>
2019-11-19 10:32:12 -0300

    Implementando form validation

---
+++ src/main/java/controller/RegisterAction.java
@@ -0,0 +1,41 @@
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import model.Person;
+
+public class RegisterAction extends ActionSupport {
+
+    private static final long serialVersionUID = 1L;
+
+    private Person personBean;
+
+    public String execute() throws Exception {
+        //call Service class to store personBean's state in database
+
+        return SUCCESS;
+    }
+
+    public Person getPersonBean() {
+        return personBean;
+    }
+
+    public void setPersonBean(Person person) {
+        personBean = person;
+    }
+
+    public void validate(){
+        if (personBean.getFirstName().length() == 0) {
+            addFieldError("personBean.firstName", "First name is required.");
+        }
+
+        if (personBean.getEmail().length() == 0) {
+            addFieldError("personBean.email", "Email is required.");
+        }
+
+        if (personBean.getAge() < 18) {
+            addFieldError("personBean.age", "Age is required and must be 18 or older");
+        }
+    }
+
+}
